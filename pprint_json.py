3216d5021bb525171b43778fba83b570e3bc4f2a
Odselmaa <d.odselaa@yahoo.com>
2016-10-13 14:33:49 +0000

    wrote by PEP8

--- pprint_json.py
+++ pprint_json.py
@@ -1,16 +1,31 @@
 import json
-from pprint import pprint
+import os
+
 
 def load_data(filepath):
-    with open(filepath) as data_file:    
-        data = json.load(data_file)
-    return data
+    if not os.path.exists(filepath):
+        return None
+    else:
+        with open(filepath) as data_file:
+            return json.load(data_file)
+
 
 def pretty_print_json(data):
-    pprint(data)
+    print(json.dumps(
+                    data,
+                    indent=4,
+                    separators=(',', ': '),
+                    ensure_ascii=False
+                    ))
 
 
 if __name__ == '__main__':
-    data = load_data("my_json.json")
-    pretty_print_json(data)
+    try:
+        file_path = input("Enter file path: ")
+    except ValueError:
+        file_path = None
+
+    if file_path is not None:
+        data = load_data(file_path)
+        pretty_print_json(data)
 
